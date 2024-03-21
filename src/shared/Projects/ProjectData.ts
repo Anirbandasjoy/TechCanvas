e9cbdcc7f62dbe274e5f1e2952d28e042e460a70
Anirbandasjoy <joy600508@gmail.com>
2024-03-21 07:48:02 +0600

    chnage project title

--- src/shared/Projects/ProjectData.ts
+++ src/shared/Projects/ProjectData.ts
@@ -7,7 +7,7 @@ export const projectsData = [
     id: 1,
     name: "NovaNexus",
     description:
-      "It is a tour-selling platform. Users can buy packages from here to give tours to different places. TailwindCSS, Flow Bite, React.js, Tan Stack, Firebase, and MongoDB  was used to build this application",
+      "NovaNexus is a social news feed website. Through which people can share different types of news. Can edit delete own post. If you want, you can save posts made by others.",
     projectVideo: image4,
     preview: "https://novanexus.vercel.app",
     source: "https://github.com/Anirbandasjoy/NovaNexus-Client",
@@ -107,7 +107,7 @@ export const projectsData = [
     id: 2,
     name: "TechCanvas",
     description:
-      "It is a tour-selling platform. Users can buy packages from here to give tours to different places. TailwindCSS, Flow Bite, React.js, Tan Stack, Firebase, and MongoDB  was used to build this application",
+      "TechCanvas is a portfolio website. Through it I present my work. I present all my personal and educational information through this website.",
     projectVideo: image3,
     preview: "https://joydas.vercel.app",
     source: "https://github.com/Anirbandasjoy/TechCanvas",
